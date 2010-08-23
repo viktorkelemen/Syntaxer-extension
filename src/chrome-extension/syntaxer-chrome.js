a2b114a30f793ae109244e75412acc24680f5594
Viktor Kelemen <viktor@cirius.co.jp>
2010-08-23 10:20:06 +0200

    Add a JS Beautifier

--- src/chrome-extension/syntaxer-chrome.js
+++ src/chrome-extension/syntaxer-chrome.js
@@ -1,28 +1,45 @@
 
-$( function () {
-        
-        
+(function () {
+
+    /**
+     * Beautifies the JavaScript source
+     */
+    function preProcessJavaScript() {
+
+        var source = $("body pre").html();
+        source = js_beautify(source);
+        $("body pre").html(source);
+    }
+
+
     var EXPS = {
         "lang-js": /\.js/,
         "lang-css": /\.css/
     };
 
+
     // selecting the className
     var path = window.location.pathname,
         className = "prettyprint",
         key;
-    
+
     for (key in EXPS) {
-        
         if (EXPS[key].test(path)) {
             className += " " + key;
             break;
         }
     }
-        
-    
+
+    // if it's JavaScript
+    if (EXPS["lang-js"].test(path)) {
+
+        // beautifier
+        preProcessJavaScript();
+    }
+
+
     $("body pre").addClass(className);
 
     prettyPrint();
 
-});
+})();
